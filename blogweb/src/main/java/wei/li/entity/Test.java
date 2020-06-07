da846bc13e900f602b02bc95ced381c73a3a6841
liwei14 <18774860316@163.com>
2020-06-07 20:09:17 +0800

    测试
    Signed-off-by:  <18774860316@163.com>

---
+++ blogweb/src/main/java/wei/li/entity/Test.java
@@ -0,0 +1,14 @@
+package wei.li.entity;
+
+/**
+ * @ProjectName: BLOG
+ * @Package: wei.li.entity
+ * @ClassName: Test
+ * @Author: HAND_WEILI
+ * @Description: ${description}
+ * @Date: 2020/6/7 19:46
+ * @Version: 1.0
+ */
+public class Test {
+    public int i;
+}
